98b961a7ce087dd193bb8aec06568ddd63d45f34
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2020-03-20 09:45:30 +0100

    Add files via upload

---
+++ Mobile first opgave komponenter/js/script.js
@@ -0,0 +1,18 @@
+// alertBox
+
+function clikker() {
+    alert("working");
+};
+
+// alertBox 
+
+function submit() {
+    alert("submitted");
+};
+
+// playing with javaScript
+
+function go() {
+    var element = document.getElementById("myDiv");
+    element.classList.toggle("newStyle");
+};
